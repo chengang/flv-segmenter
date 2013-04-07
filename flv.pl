1a64ef5044d609f4d4c2d8ac5358b8868c918530
chengang <chengang@cpan.org>
2013-04-07 14:04:39 +0800

    add more hashs

--- flv.pl
+++ flv.pl
@@ -5,7 +5,64 @@ my %tag_type =
 (
 	8  => 'audio',
 	9  => 'video',
-	18 => 'ScriptData',
+	18 => 'script',
+);
+
+my %audio_format = 
+(
+   0  => 'uncompressed',
+   1  => 'ADPCM',
+   2  => 'MP3',
+   3  => 'Linear PCM, little endian',
+   4  => 'Nellymoser 16kHz mono',
+   5  => 'Nellymoser 8kHz mono',
+   6  => 'Nellymoser',
+   7  => 'G.711 A-law',
+   8  => 'G.711 mu-law',
+   10 => 'AAC',
+   11 => 'Speex',
+   14 => 'MP3 8kHz',
+   15 => 'Device-specific sound',
+);
+
+my %audio_rate = 
+(
+   0 => '5518Hz',
+   1 => '11025Hz',
+   2 => '22050Hz',
+   3 => '44100Hz',
+);
+
+my %audio_size = 
+(
+   0 => '8bit',
+   1 => '16bit',
+);
+
+my %audio_type = 
+(
+   0 => 'mono',
+   1 => 'stereo',
+);
+
+my %video_codec =
+(
+   1 => 'JPEG',
+   2 => 'Sorenson H.263',
+   3 => 'Screen video',
+   4 => 'On2 VP6',
+   5 => 'On2 VP6 + alpha',
+   6 => 'Screen video v2',
+   7 => 'AVC',
+);
+
+my %video_type =
+(
+   1 => 'keyframe',
+   2 => 'interframe',
+   3 => 'disposable interframe',
+   4 => 'generated keyframe',
+   5 => 'video info/command frame',
 );
 
 open FH, "<", shift;
@@ -26,7 +83,7 @@ say "Header Size: " . unpack("N",$buf);
 
 sysread(FH, $buf, 4); # jump PreviousTagSize0
 
-print "Tag Type\tData Size\tTimestamp\n";
+say "Tag Type\tData Size\tTimestamp\tFormat\ttype";
 while( sysread(FH, $buf, 8) )
 {
 	my ($tag_type, $data_size, $ts, @datasize, @timestamp);
@@ -46,11 +103,26 @@ while( sysread(FH, $buf, 8) )
 
 	if ($tag_type eq 'audio')
 	{
-		#my $flag = 1;
+		my $flags = unpack 'C', substr($buf, 0, 1);
+		my $format = (($flags >> 4) & 0x0f);
+		my $rate   = (($flags >> 2) & 0x03);
+		my $size   = (($flags >> 1) & 0x01);
+		my $type   = $flags & 0x01;
+		my $audio_format = $audio_format{$format};
+		my $audio_rate = $audio_rate{$rate};
+		my $audio_size = $audio_size{$size};
+		my $audio_type = $audio_type{$type};
+		$info .= "\t$audio_format\t$audio_type\t$audio_rate\t$audio_size";
 		$print->yellow($info);
 	}
 	elsif ($tag_type eq 'video')
 	{
+		my $flags = unpack 'C', substr($buf, 0, 1);
+		my $type  = ($flags >> 4) & 0x0f;
+		my $codec = $flags & 0x0f;
+		my $video_type = $video_type{$type};
+		my $video_codec = $video_codec{$codec};
+		$info .= "\t$video_codec\t$video_type";
 		$print->red($info);
 	}
 	else
@@ -60,5 +132,4 @@ while( sysread(FH, $buf, 8) )
 
 	sysread(FH, $buf, 4); # jump PreviousTagSize
 }
-
 
